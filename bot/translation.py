e5b1085c4848f4ab16c14a4f73f345b5bdd46864
emoboy-man <84627196+emoboy-man@users.noreply.github.com>
2021-06-29 00:15:40 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,55 +4,29 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey {} what's up mahn 😎!!</b>
+<i><b>Am A Advance Auto Filter Bot For MallusHubb....😎
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+MallusHubb Official Auto Filter Bot 🔥🔥🎬
 
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+Join Our Main Channel 👇🏻..
+@MallusHubb
+</b></i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@CrazyBotsz
+<b><n>ഈ ബോട്ട് ©Mallu Hub Movies ഗ്രൂപ്പിനു വേണ്ടി നിർമിച്ചതാണ്.Start കൊടുത്തു വെറുതെ സമയം പാഴാക്കല്ലേ സുഹൃത്തേ..!നിങ്ങൾ പാഴക്കികളയുന്ന ഓരോ ടൈമും വിലപ്പെട്ടതാണ്..😌</n></b>
+@MallusHubb
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/vaathi_comin_g">vaathi_comin_g</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ YouTube</b> : <i><a href="https://youtube.com/c/LOStWorldBGM">subscribe YouTube channel </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/vaathi_comin_g">Click Me</a></i>
 """
