5d0a4474710a9b0e7a0403dfbafb63f1a8fcea93
MALLU HUB MOVIES <84627196+emoboy-man@users.noreply.github.com>
2021-06-29 10:24:01 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} <b><i>💠 Results For Your Query ♻️:</i></b> <code>{query}</code> 👇🏻 /n/n <b>Ⓜ️ MALLU HUB MOVIES 💠</b>",
+                text=f"Found {(len_results)} <b><i>💠 Results For Your Query ♻️:</i></b> <code>{query}</code> 👇🏻 \n\n<b>Ⓜ️ MALLU HUB MOVIES 💠</b>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
